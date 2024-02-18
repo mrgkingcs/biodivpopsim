789f4e1ec99b52554c06eac9e442dc899b05815e
mrgkingcs <mrgkingcs@gmail.com>
2024-02-18 12:41:51 +0000

    Implemented updating of population plots when model has updated data

--- timeseriesgraphview.py
+++ timeseriesgraphview.py
@@ -33,12 +33,22 @@ class TimeSeriesGraphView(BaseView):
 
         # create the matplotlib link
         self.__figure = Figure(dpi=TimeSeriesGraphView.DPI)
-        self.__graph = FigureCanvasTkAgg(self.__figure, self.__graphCanvas)
 
+        self.__graph = FigureCanvasTkAgg(self.__figure, self.__graphCanvas)
         self.__graph.get_tk_widget().bind("<Configure>", lambda event: self.canvasResized(event))
 
         self.__plot = self.__figure.add_subplot(1, 1, 1)
         self.__plot.set_aspect('auto')
+        self.__plot.spines['top'].set_visible(False)
+        self.__plot.spines['right'].set_visible(False)
+
+        self.__plot.set_title(seriesID)
+        self.__plot.set_xlabel("Year")
+        self.__plot.set_xlim([0, 10])
+        self.__plot.set_ylabel("Population")
+        self.__plot.set_ylim([0, 10])
+
+        self.__populationLine, = self.__plot.plot([0, 1], [0, 0]) # apparently plot returns a tuple
 
         self.__graph.get_tk_widget().pack()
 
@@ -49,20 +59,42 @@ class TimeSeriesGraphView(BaseView):
 
     def timeSeriesUpdated(self, seriesData):
         """Re-plot time series to canvas"""
-        pass
+
+        popValues = seriesData["seriesValues"]
+
+        # calc x bounds
+        startYear, endYear = seriesData["startYear"], seriesData["endYear"]
+        upperXBound = endYear
+        if endYear - startYear < 10:
+            upperXBound = startYear + 10
+        self.__plot.set_xlim([startYear, upperXBound])
+
+        # calc y bounds
+        maxPop = max(popValues)
+        lowerYBound, upperYBound = self.__plot.get_ylim()
+        while upperYBound < maxPop:
+            upperYBound *= 10
+        self.__plot.set_ylim([lowerYBound, upperYBound])
+
+        # update the data
+        self.__populationLine.set_xdata(list(range(startYear, endYear+1)))
+        self.__populationLine.set_ydata(popValues)
+
+        self.__graph.draw()
+        self.__graph.flush_events()
 
     def canvasResized(self, event):
-        print(f"Resized canvas to:{event.width},{event.height}")
+        #print(f"Resized canvas to:{event.width},{event.height}")
+        pass
 
     def widgetResized(self, event):
-        print(f"Resizing widget to:{event.width},{event.height}")
+        #print(f"Resizing widget to:{event.width},{event.height}")
         self.__figure.set_size_inches(event.width/TimeSeriesGraphView.DPI,
                                       event.height/TimeSeriesGraphView.DPI)
-        print("Overriding canvas size")
+        #print("Overriding canvas size")
         self.__graph.get_tk_widget().config(width=event.width, height=event.height)
-        print("Calling resize on graph")
+        #print("Calling resize on graph")
         self.__graph.resize(event)
-        print("Calling draw on graph")
+        #print("Calling draw on graph")
         self.__graph.draw()
-        #self.__graph.flush_events()
 
